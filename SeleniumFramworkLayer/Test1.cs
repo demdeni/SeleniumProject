bd5564956d24e98d2ba2dc3da808e116f0f3634a
demdeni <demdeni@gmail.com>
2018-02-25 13:43:24 +0200

    change with chrome driver in test

--- SeleniumFramworkLayer/Test1.cs
+++ SeleniumFramworkLayer/Test1.cs
@@ -7,6 +7,8 @@ using NUnit.Framework;
 using OpenQA.Selenium.Firefox;
 using System.IO;
 using System.Reflection;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace SeleniumFramworkLayer
 {
@@ -15,7 +17,10 @@ namespace SeleniumFramworkLayer
         [Test]
         public static void RunTest()
         {
-            
+            IWebDriver driver1 = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver1.Manage().Window.Maximize();
+
+            driver1.Navigate().GoToUrl("https://syneron-candela.com");
         }
     }
 }
