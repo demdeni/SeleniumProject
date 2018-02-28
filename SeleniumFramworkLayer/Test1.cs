6e5171697e594bf6fe415f72f0cee4a6d8e3ac07
demdeni <demdeni@gmail.com>
2018-02-28 09:36:12 +0200

    222

--- SeleniumFramworkLayer/Test1.cs
+++ SeleniumFramworkLayer/Test1.cs
@@ -15,10 +15,11 @@ namespace SeleniumFramworkLayer
     public class Test1 : TestBase
     {
         [Test]
+        
         public static void RunTest()
         {
             IWebDriver driver1 = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            driver1.Manage().Window.Maximize();
+            //driver1.Manage().Window.Maximize();
 
             driver1.Navigate().GoToUrl("https://syneron-candela.com");
         }
