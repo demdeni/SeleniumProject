c751aea5843fd58529675761d75b36e793425fde
demdeni <demdeni@gmail.com>
2018-03-05 13:36:56 +0200

    test in base class

--- SeleniumFramworkLayer/TestBase.cs
+++ SeleniumFramworkLayer/TestBase.cs
@@ -31,8 +31,35 @@ namespace SeleniumFramworkLayer
 
             //driver.Navigate().GoToUrl("https://syneron-candela.com");
 
-        }        
-        
+        }
+
+        private static string baseURL = "https://syneron-candela.com/int";
+        //public static IWebDriver driver;
+        //public RemoteWebDriver driver1;
+        //private string browser;
+        public TestContext TestContext { get; set; }
+
+        [Test]
+        public static void RunTestBaseClass()
+        {
+            //ChromeOptions option = new ChromeOptions();
+            //option.AddArgument("--headless");
+            //driver = new ChromeDriver(option);
+
+            driver.Navigate().GoToUrl(baseURL);
+
+            var element = driver.FindElement(By.ClassName("wlcm-title"));
+            Assert.IsTrue(element.Displayed);
+
+            driver.FindElement(By.Id("cboxClose")).Click();
+
+            var logo = driver.FindElement(By.Id("logo"));
+            Assert.IsTrue(logo.Displayed);
+            var element1 = driver.FindElement(By.ClassName("sgfgsfgs"));
+            Assert.IsNotNull(element1);
+            driver.Quit();
+        }
+
         public void TestExecution()
         {
             TestInitialize();
